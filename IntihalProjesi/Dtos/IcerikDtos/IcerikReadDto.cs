e2d5d51797eb8fcb4bef924bc0f95c24b6d0176d
emre almamis <ealmamis56@gmail.com>
2025-03-11 15:56:44 +0300

    Öğrenci panelinde ödev gönderme tamamlandı

--- IntihalProjesi/Dtos/IcerikDtos/IcerikReadDto.cs
+++ IntihalProjesi/Dtos/IcerikDtos/IcerikReadDto.cs
@@ -2,7 +2,7 @@
 {
     public class IcerikReadDto
     {
-        //public int IcerikId { get; set; } // Ödev ID'si
+        public int IcerikId { get; set; } // Ödev ID'si
         public string Baslik { get; set; } // Ödev Başlığı
         public string Aciklama { get; set; } // Ödev Açıklaması
         public DateTime OlusturmaTarihi { get; set; } // Ödev Oluşturma Tarihi
